7a35e2260950e3af9312f8fcdca40574067c045b
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2024-03-23 16:55:29 -0500

    Add logic to randomly select rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+// This function returns either
+// 'Rock', 'Paper', or 'Scissors'
+// based on the input
+function getComputerChoice() {
+    let x = Math.floor(Math.random() * (4 - 1) + 1);
+    if (x == 1) {
+        console.log("Rock");
+    }
+    if (x == 2) {
+        console.log("Paper");
+    }
+    if (x == 3) {
+        console.log("Scissors")
+    }
+}
+
+getComputerChoice();
