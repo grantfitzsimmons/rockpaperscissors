b64c1b31e78bd8824f763f0d349b3725f8ad31cf
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2024-03-23 17:05:28 -0500

    Add initial logic for player selection

--- script.js
+++ script.js
@@ -4,14 +4,29 @@
 function getComputerChoice() {
     let x = Math.floor(Math.random() * (4 - 1) + 1);
     if (x == 1) {
-        console.log("Rock");
+        return ("rock");
     }
     if (x == 2) {
-        console.log("Paper");
+        return ("paper");
     }
     if (x == 3) {
-        console.log("Scissors")
+        return ("scissors");
     }
 }
 
-getComputerChoice();
+// This calls the function that returns either
+// 'Rock', 'Paper', or 'Scissors'
+let computerSelection = getComputerChoice()
+
+console.log(computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt()
+    if (playerSelection == 'rock') {
+        console.log("player chose rock")
+    }
+}
+
+let playerSelection;
+playRound();
+
